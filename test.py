75fd54dce1685ea278d6eda393d5b091b70fce1e
PaoloReyes <paolo.alfonso.reyes@gmail.com>
2024-07-26 14:49:59 -0600

    I did not remember binary

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ try:
     while True:
         client.write_register(address=512, value=1, unit=1)
         time.sleep(2)
-        client.write_register(address=512, value=3, unit=1)
+        client.write_register(address=512, value=2, unit=1)
         time.sleep(2)
         print("Cycle Completed")
 # result = client.read_holding_registers(address=0, count=1, slave=1) # get information from IO
