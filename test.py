9d5515003ddc3da1418d5523d88c153e107b1a0a
PaoloReyes <paolo.alfonso.reyes@gmail.com>
2024-07-26 14:33:22 -0600

    debugging

--- test.py
+++ test.py
@@ -11,6 +11,7 @@ try:
         client.write_register(address=512, value=0, unit=1)
         client.write_register(address=513, value=1, unit=1)
         time.sleep(2)
+        print("Cycle Completed")
 # result = client.read_holding_registers(address=0, count=1, slave=1) # get information from IO
 # print(result) # use information
 finally:
